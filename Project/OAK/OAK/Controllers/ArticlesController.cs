e8fe38304f2619c8941b306be425214b44c3a864
BartBurton <bburtonZero@gmail.com>
2021-03-23 02:47:33 +0300

    oak_dev_0.2.7.1

--- Project/OAK/OAK/Controllers/ArticlesController.cs
+++ Project/OAK/OAK/Controllers/ArticlesController.cs
@@ -29,13 +29,14 @@ namespace OAK.Controllers
 
 
 
-        public IActionResult News(int start = 0)
+        public async Task<IActionResult> News(int start = 0)
         {
             start = (start >= 0) ? start : 0;
-
+            ViewBag.Start = start;
+            ViewBag.Action = "News";
             ViewBag.Title = "Статьи ДУБа";
 
-            var articles = _oak.Articles
+            var articles = await _oak.Articles
                 .OrderByDescending(a => a.Date)
                 .Skip(start * _countOfEl)
                 .Take(_countOfEl)
@@ -43,51 +44,102 @@ namespace OAK.Controllers
                 .Include(a => a.IdsectionNavigation)
                 .Include(a => a.ArtTexts.Take(1))
                 .Include(a => a.ArtImages.Take(1))
-                .ToList();
-
-            ViewBag.Start = start; 
+                .ToListAsync();
 
 
-
-            return View(articles);
+            return View("Articles", articles);
         }
 
 
-        public IActionResult CreatedArticles(long? id, int start = 0)
+        public async Task<IActionResult> CreatedArticles(long? id, int start = 0)
         {
             start = (start >= 0) ? start : 0;
             ViewBag.Start = start;
+            ViewBag.Action = "CreatedArticles";
+            ViewBag.Title = "Статьи ДУБа";
 
+            var articles = await _oak.Articles
+                .Where(a => a.Idautor == id)
+                .OrderByDescending(a => a.Date)
+                .Skip(start * _countOfEl)
+                .Take(_countOfEl)
+                .Include(a => a.IdautorNavigation)
+                .Include(a => a.IdsectionNavigation)
+                .Include(a => a.ArtTexts.Take(1))
+                .Include(a => a.ArtImages.Take(1))
+                .ToListAsync();
 
-            return View();
+            return View("Articles", articles);
         }
 
 
-        public IActionResult FavoriteArticles(long? id, int start = 0)
+        public async Task<IActionResult> FavoriteArticles(long? id, int start = 0)
         {
             start = (start >= 0) ? start : 0;
             ViewBag.Start = start;
+            ViewBag.Action = "FavoriteArticles";
+            ViewBag.Title = "Статьи ДУБа";
 
-            return View();
+            var articles = await _oak.Articles
+                .Where(a => a.FavArticles.Any(fa => fa.Idarticle == a.Id))
+                .OrderByDescending(a => a.Date)
+                .Skip(start * _countOfEl)
+                .Take(_countOfEl)
+                .Include(a => a.IdautorNavigation)
+                .Include(a => a.IdsectionNavigation)
+                .Include(a => a.ArtTexts.Take(1))
+                .Include(a => a.ArtImages.Take(1))
+                .ToListAsync();
+
+            return View("Articles", articles);
         }
 
 
-        public IActionResult ArticlesSection(long? id, int start = 0)
+        public async Task<IActionResult> ArticlesSection(long? id, int start = 0)
         {
             start = (start >= 0) ? start : 0;
             ViewBag.Start = start;
+            ViewBag.Action = "ArticlesSection";
+            ViewBag.Title = "Статьи ДУБа";
 
-            return View();
+            var articles = await _oak.Articles
+                .Where(a => a.Idsection == id)
+                .OrderByDescending(a => a.Date)
+                .Skip(start * _countOfEl)
+                .Take(_countOfEl)
+                .Include(a => a.IdautorNavigation)
+                .Include(a => a.IdsectionNavigation)
+                .Include(a => a.ArtTexts.Take(1))
+                .Include(a => a.ArtImages.Take(1))
+                .ToListAsync();
+
+            return View("Articles", articles);
         }
 
 
-        public IActionResult NewsFavorite(long? id, int start = 0)
+        public async Task<IActionResult> NewsFavorite(long? id, int start = 0)
         {
             start = (start >= 0) ? start : 0;
             ViewBag.Start = start;
+            ViewBag.Action = "NewsFavorite";
+            ViewBag.Title = "Статьи ДУБа";
 
+            var autors = _oak.FavAutors
+                //.Where(fa => fa.Idautororigin == id)
+                .Select(fa => fa.Idautororigin == id)
+                .ToListAsync();
 
-            return View();
+            var articles = await _oak.Articles
+                .OrderByDescending(a => a.Date)
+                .Skip(start * _countOfEl)
+                .Take(_countOfEl)
+                .Include(a => a.IdautorNavigation)
+                .Include(a => a.IdsectionNavigation)
+                .Include(a => a.ArtTexts.Take(1))
+                .Include(a => a.ArtImages.Take(1))
+                .ToListAsync();
+                
+            return View("Articles", articles);
         }
     }
 }
